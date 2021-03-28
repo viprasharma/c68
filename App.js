6beab6bec791c91797e89dc231c925f2c5728d24
vipra <vipra.sharma06@gmail.com>
2021-03-28 11:03:16 +0530

    done

--- App.js
+++ App.js
@@ -1,13 +1,24 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
+import {createAppContainer} from 'react-navigation';
+import {createBottomTabNavigation} from 'react-navigation-tabs';
+import TransactionScreen from './screen/TransactionScreen.js';
+import SearchScreen from './screen/SearchScreen.js';
+export default class App extends React.Component {
+  
+  render()
+  {
+    return (
+    <AppContainer/>
   );
 }
+}
+const TabNavigator = createBottomTabNavigator({
+  Transaction: {screen: TransactionScreen},
+  Search: {screen: SearchScreen},
+});
+
+const AppContainer =  createAppContainer(TabNavigator);
 
 const styles = StyleSheet.create({
   container: {
